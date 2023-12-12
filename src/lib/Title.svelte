159643cf4a49df0243e907fcd10102c8c3027788
newtondotcom <asphalt9fr@gmail.com>
2023-12-12 23:38:57 +0100

    generic title sub

---
+++ src/lib/Title.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let title: string;
+    export let subtitle:string;
+</script>
+
+<div class="mt-8 ml-10 mb-8">
+    <h1 class="text-2xl font-bold text-gray-900 sm:text-3xl">
+      {title}
+    </h1>
+
+    <p class="mt-1.5 text-sm text-gray-500">
+        {subtitle}
+    </p>
+</div>
