5606d91f88b69008f28d5c83ae8aea44414bf23f
newtondotcom <asphalt9fr@gmail.com>
2023-12-12 23:26:25 +0100

    v1 main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="h-[250px]"></div>
+
+<section class="overflow-hidden bg-gray-800 sm:grid sm:grid-cols-2">
+    <div class="p-8 md:p-12 lg:px-16 lg:py-24">
+      <div class="mx-auto max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
+        <h2 class="text-2xl font-bold text-gray-100 md:text-3xl">
+            Single-take walkthrough videos
+            that stand out.
+        </h2>
+  
+        <p class="hidden text-gray-200 md:mt-4 md:block">
+            We fly small drones indoors creating seamless walkthrough videos for real estate, businesses, hotels, & more.
+        </p>
+  
+        <div class="mt-4 md:mt-8">
+          <button
+            class="inline-block rounded bg-emerald-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-emerald-700 focus:outline-none focus:ring focus:ring-yellow-400"
+          >
+            Get Started Today
+          </button>
+        </div>
+      </div>
+    </div>
+  
+    <img
+      alt="Student"
+      src="/camera.gif"
+      class="h-56 w-full object-cover sm:h-full"
+    />
+</section>
+
+<div class="flex flex-col justify-center mx-auto">
+    <img 
+    src="/avata.jpg"
+    alt="test"
+    class="w-2/3 mx-auto"
+    >
+</div>
+
+  
