f7bc899d9db265b3860b5786ab00459ad93c7127
newtondotcom <asphalt9fr@gmail.com>
2023-12-17 17:25:03 +0100

    same links than header

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -12,27 +12,19 @@
   
       <ul class="mt-12 flex flex-wrap justify-center gap-6 md:gap-8 lg:gap-12">
         <li>
-          <a class="text-gray-400 transition hover:text-gray-400/75" href="/"> About </a>
+          <button class="text-gray-400 transition hover:text-gray-500/75" on:click={()=>goto("/")}> Home </button>
         </li>
-  
+
         <li>
-          <a class="text-gray-400 transition hover:text-gray-400/75" href="/"> Careers </a>
+          <button class="text-gray-400 transition hover:text-gray-500/75" on:click={()=>goto("/services")}> Services </button>
         </li>
-  
+
         <li>
-          <a class="text-gray-400 transition hover:text-gray-400/75" href="/"> History </a>
+          <button class="text-gray-400 transition hover:text-gray-500/75" on:click={()=>goto("/portfolio")}> Portfolio </button>
         </li>
-  
+
         <li>
-          <a class="text-gray-400 transition hover:text-gray-400/75" href="/"> Services </a>
-        </li>
-  
-        <li>
-          <a class="text-gray-400 transition hover:text-gray-400/75" href="/"> Projects </a>
-        </li>
-  
-        <li>
-          <a class="text-gray-400 transition hover:text-gray-400/75" href="/"> Blog </a>
+          <button class="text-gray-400 transition hover:text-gray-500/75" on:click={()=>goto("/qa")}> FAQ </button>
         </li>
       </ul>
   
@@ -42,7 +34,7 @@
             href="/"
             rel="noreferrer"
             target="_blank"
-            class="text-gray-400 transition hover:text-gray-400/75"
+            class="text-gray-400 transition hover:text-gray-500/75"
           >
             <span class="sr-only">Instagram</span>
             <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
@@ -60,7 +52,7 @@
             href="/"
             rel="noreferrer"
             target="_blank"
-            class="text-gray-400 transition hover:text-gray-400/75"
+            class="text-gray-400 transition hover:text-gray-500/75"
           >
             <span class="sr-only">Instagram</span>
             <svg class="h-6 w-6" fill="currentColor" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>LinkedIn</title><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/></svg>
@@ -72,7 +64,7 @@
             href="/"
             rel="noreferrer"
             target="_blank"
-            class="text-gray-400 transition hover:text-gray-400/75"
+            class="text-gray-400 transition hover:text-gray-500/75"
           >
             <span class="sr-only">GitHub</span>
             <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
