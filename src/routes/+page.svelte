2335edf8c2f08285513e09fc72243c06e4f30b5e
newtondotcom <asphalt9fr@gmail.com>
2023-12-26 12:00:33 +0100

    ui tweaks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,12 @@
   import Footer from '$lib/Footer.svelte';
   import Divider from '$lib/Divider.svelte';
   import translate from '$lib/locales/function';
+    import { goto } from "$app/navigation";
+
+  const titleColor = "#fff";
+    const titleShadowColor = "#000";
+    const titleFontFamily = "Montserrat";
+
 </script>
 
 <svelte:head>
@@ -32,6 +38,7 @@
       <div class="mt-4 md:mt-8">
         <button
           class="inline-block rounded bg-emerald-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-emerald-700 focus:outline-none focus:ring focus:ring-yellow-400"
+          on:click={()=>goto("/contact")}
         >
           {translate("get_started")}
         </button>
@@ -73,5 +80,16 @@
 
 <div class="flex flex-col text-white lg:w-4/5 mx-auto py-4 msm:px-4">
     <div class="flex self-center my-8 text-5xl">SHOWREEL</div>
-    <Youtube id="zHKfHjk8EiY" />
+    <Youtube id="mS7L6ite8TI"
+    --title-color={titleColor}
+    --title-shadow-color="{titleShadowColor}100"
+    --title-font-family={titleFontFamily}
+    --overlay-bg-color="hsla(0, 0%, 0%, 0.3)" 
+    --overlay-transition="all 100ms linear" 
+    animations={false}
+    >
+        <button class="text-black">
+            <svg viewBox="0 0 24 24" height="40" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22ZM10.6935 15.8458L15.4137 13.059C16.1954 12.5974 16.1954 11.4026 15.4137 10.941L10.6935 8.15419C9.93371 7.70561 9 8.28947 9 9.21316V14.7868C9 15.7105 9.93371 16.2944 10.6935 15.8458Z" fill="currentColor"></path> </g></svg>
+        </button>
+    </Youtube>
 </div>
