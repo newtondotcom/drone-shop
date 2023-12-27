a15339e7fa3df74597101622fd7e3696173519ba
newtondotcom <asphalt9fr@gmail.com>
2023-12-28 00:30:49 +0100

    version before chatgpt trans

--- src/lib/Faq.svelte
+++ src/lib/Faq.svelte
@@ -5,16 +5,6 @@
     let questions = [
         {
             "en": {
-                question: "What is the turnaround time for a drone tour?",
-                answer: "Most drone tour videos are delivered within 24 hours. Turnaround times for other drone tour videos are typically within 72 hours but vary depending on the project."
-            },
-            "fr": {
-                question: "Quel est le délai de livraison d'une visite en drone ?",
-                answer: "La plupart des vidéos de visite en drone sont livrées en moins de 24 heures. Les délais de livraison pour d'autres vidéos de visite en drone sont généralement de 72 heures, mais varient en fonction du projet."
-            }
-        },
-        {
-            "en": {
                 question: "What's the benefit of a drone tour vs a regular video tour?",
                 answer: "Since drone video tours are shot entirely in one take, viewers have a much better sense of the layout and size of a property as opposed to standard videos. We are also able to achieve camera angles that would be otherwise impossible."
             },
@@ -25,22 +15,12 @@
         },
         {
             "en": {
-                question: "Are there safety concerns flying a drone indoors?",
-                answer: "We fly drones that are custom built and tuned specifically for flying slow indoors. They weigh less than .55 lbs and have ducts around the propellers making them extremely safe to fly indoors."
-            },
-            "fr": {
-                question: "Y a-t-il des préoccupations en matière de sécurité lors de la conduite d'un drone en intérieur ?",
-                answer: "Nous utilisons des drones spécialement conçus et réglés pour voler lentement en intérieur. Ils pèsent moins de 0,55 livre et sont équipés de conduits autour des hélices, ce qui les rend extrêmement sûrs pour le vol en intérieur."
-            }
-        },
-        {
-            "en": {
                 question: "What kind of properties do you film?",
                 answer: "We film everything from residential real estate and small businesses to venues and corporate office buildings. No project is too small or too big."
             },
             "fr": {
                 question: "Quels types de propriétés filmez-vous ?",
-                answer: "Nous filmens tout, des biens immobiliers résidentiels et des petites entreprises aux lieux et aux immeubles de bureaux d'entreprise. Aucun projet n'est trop petit ou trop grand."
+                answer: "Nous filmons tout : des biens immobiliers résidentiels et des petites entreprises aux lieux et aux immeubles de bureaux d'entreprise. Aucun projet n'est trop petit ou trop grand."
             }
         },
         {
@@ -50,7 +30,7 @@
             },
             "fr": {
                 question: "Combien de temps faut-il pour filmer ?",
-                answer: "Les visites en drone résidentielles prennent généralement environ 30 à 60 minutes à filmer."
+                answer: "Les visites de drone résidentielles prennent généralement environ 30 à 60 minutes à filmer."
             }
         },
         {
@@ -60,7 +40,7 @@
             },
             "fr": {
                 question: "Quel est le délai de préavis pour planifier mon vol ?",
-                answer: "Vous pouvez généralement planifier votre vol commercial dans un délai de 7 jours. Vous pouvez généralement planifier votre vol résidentiel dans un délai de 2 jours."
+                answer: "Vous pouvez généralement planifier votre vol commercial dans un délai de 7 jours avant la date prévue. Vous pouvez généralement planifier votre vol en secteur résidentiel dans un délai de 2 jours."
             }
         },
         {
@@ -75,6 +55,16 @@
         },
         {
             "en": {
+                question: "What is the turnaround time for a drone tour?",
+                answer: "Most drone tour videos are delivered within 24 hours. Turnaround times for other drone tour videos are typically within 72 hours but vary depending on the project."
+            },
+            "fr": {
+                question: "Quel est le délai de livraison de la vidéo d'une visite en drone ?",
+                answer: "La plupart des vidéos de visite en drone sont livrées en moins de 24 heures. Les délais de livraison pour d'autres vidéos de visite en drone sont généralement de 72 heures, mais varient en fonction du projet."
+            }
+        },
+        {
+            "en": {
                 question: "What are the rates?",
                answer: "The rates are defined according to the duration of the service, the number of places to film and the complexity of the shots to be made. For a personalized quote, contact us. Indicative prices are available on the Services page."
             },
@@ -85,22 +75,22 @@
         },
         {
             "en": {
+                question: "Are there safety concerns flying a drone indoors?",
+                answer: "We fly drones that are custom built and tuned specifically for flying slow indoors. They weigh less than .55 lbs and have ducts around the propellers making them extremely safe to fly indoors."
+            },
+            "fr": {
+                question: "Y a-t-il des préoccupations en matière de sécurité lors de la conduite d'un drone en intérieur ?",
+                answer: "Nous utilisons des drones spécialement conçus et réglés pour voler lentement en intérieur. Ils pèsent moins de 0,55 livre et sont équipés de conduits autour des hélices, ce qui les rend extrêmement sûrs pour le vol en intérieur."
+            }
+        },
+        {
+            "en": {
                 question: "Where are we located?",
                answer: "We can perform services in the Paris region or around Toulouse. For services outside these areas, contact us. Travel expenses may apply."
             },
             "fr":{
                 question: "Où sommes nous situés ?",
                answer: "Nous pouvons effectuer des prestations en région parisienne ou aux alentours de Toulouse. Pour des prestations en dehors de ces zones, contactez-nous. Des fras de déplacement peuvent s'appliquer."
-            },
-        },
-        {
-            "en":{
-                question : " ",
-                answer : " "
-            },
-            "fr": {
-                question : "Pourquoi faire une vidéo et pas de simples photos ?",
-                answer : "De mon point de vue, ce qui rend le mieux, c’est de faire un plan séquence, c’est-à-dire de faire la visite de la maison en une prise, comme les vidéos disponibles sur mon site. Contrairement à de simples photos des différentes pièces prises par un photographe par exemple, le téléspectateur peut se rendre compte des volumes de la maison et de la disposition des pièces."
             },
         },
     ];
