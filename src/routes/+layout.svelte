5898e6dccd48f14fb0ced9e6186cd9c26688cca7
newtondotcom <asphalt9fr@gmail.com>
2024-02-01 14:39:11 +0100

    very satisfying version

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -102,7 +102,7 @@
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5"
+                class="h-5 w-5 text-emerald-600"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
