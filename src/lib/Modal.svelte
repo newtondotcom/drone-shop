3adf240ab287fe0a678029ea60f0f80e39be6180
newtondotcom <asphalt9fr@gmail.com>
2024-02-08 09:14:33 +0100

    hide modal on mobile

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -43,7 +43,7 @@
 </script>
 
 {#if open}
-<div class="modal z-50 fixed w-full h-full top-0 left-0 flex items-center justify-center p-8 lg:p-0">
+<div class="modal z-50 fixed w-full h-full top-0 left-0 flex items-center justify-center p-8 lg:p-0 msm:hidden">
   <div class="modal-overlay fixed w-full h-full bg-gray-900 opacity-50"></div>
   <div class="bg-white w-full lg:h-max lg:w-1/2 mx-auto rounded-lg shadow-xl z-50 overflow-y-auto">
     <div class="flex justify-center items-center head bg-gray-100 py-5 px-8 text-2xl font-extrabold">
