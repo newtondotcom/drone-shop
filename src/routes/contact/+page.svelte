d721b811ff307d07f18b49ac121ec045ac176a21
newtondotcom <asphalt9fr@gmail.com>
2024-07-18 00:00:08 +0200

    feat : contact page rewrote

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -2,6 +2,35 @@
 	import { sendToDiscordWebhook } from './discord';
 	import translate from '$lib/locales/function';
 	import { goto } from '$app/navigation';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import * as Card from '$lib/components/ui/card/index.js';
+	import * as Select from '$lib/components/ui/select/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { Label } from '$lib/components/ui/label/index.js';
+	import { Textarea } from '$lib/components/ui/textarea/index.js';
+
+	const services = [
+		{
+			value: 'residential',
+			label: 'Residential'
+		},
+		{
+			value: 'local_business',
+			label: 'Local Business'
+		},
+		{
+			value: 'office',
+			label: 'Office'
+		},
+		{
+			value: 'hostel',
+			label: 'Hostel'
+		},
+		{
+			value: 'construction',
+			label: 'Construction'
+		}
+	];
 
 	let name = '';
 	let email = '';
@@ -46,104 +75,73 @@
 			</p>
 		</div>
 	{:else}
-		<div class="-mt-20 flex flex-col self-center pb-4 pt-8 align-middle">
-			<h1 class="text-2xl font-bold text-gray-100 sm:text-3xl">
-				{translate('contactUs')}
-			</h1>
-
-			<p class="mt-1.5 text-sm text-gray-500">
-				{translate('responseMessage')}
-			</p>
-		</div>
-
-		<form on:submit={handleSubmit} autocomplete="off" class="flex flex-col">
-			<div class="mt-4 flex self-center align-middle">
-				<label
-					for="UserName"
-					class="block overflow-hidden rounded-md border border-gray-200 bg-slate-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
-				>
-					<span class="text-xs font-medium text-gray-700">{translate('fullName')}</span>
-					<input
-						type="text"
-						id="UserName"
-						placeholder={translate('fullNamePlaceholder')}
-						class="mt-1 w-full border-none bg-slate-200 p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
-						bind:value={name}
-					/>
-				</label>
-			</div>
-
-			<div class="mt-4 flex self-center align-middle">
-				<label
-					for="UserEmail"
-					class="block overflow-hidden rounded-md border border-gray-200 bg-slate-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
-				>
-					<span class="text-xs font-medium text-gray-700">{translate('email')}</span>
-					<input
-						type="email"
-						id="UserEmail"
-						placeholder={translate('emailPlaceholder')}
-						class="mt-1 w-full border-none bg-slate-200 p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
-						bind:value={email}
-					/>
-				</label>
-			</div>
-
-			<div class="mt-4 flex self-center align-middle">
-				<label
-					for="UserPhone"
-					class="block overflow-hidden rounded-md border border-gray-200 bg-slate-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
-				>
-					<span class="text-xs font-medium text-gray-700">{translate('phoneNumber')}</span>
-					<input
-						type="tel"
-						id="UserPhone"
-						placeholder={translate('phonePlaceholder')}
-						class="mt-1 w-full border-none bg-slate-200 p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
-						bind:value={phone}
-					/>
-				</label>
-			</div>
-
-			<div class="mt-4 flex self-center">
-				<select
-					id="projects"
-					bind:value={project}
-					class="block rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-				>
-					<option selected>{translate('choose_service')}</option>
-					<option value="residential">{translate('residential')}</option>
-					<option value="local_business">{translate('local_business')}</option>
-					<option value="office">{translate('office')}</option>
-					<option value="hostel">{translate('hostel')}</option>
-					<option value="construction">{translate('construction')}</option>
-					<option value="industrial">{translate('industrial')}</option>
-				</select>
-			</div>
-
-			<div class="mt-4 flex self-center align-middle">
-				<label
-					for="UserProject"
-					class="block overflow-hidden rounded-md border border-gray-200 bg-slate-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
-				>
-					<span class="text-xs font-medium text-gray-700">{translate('project')}</span>
-					<textarea
-						id="UserProject"
-						placeholder={translate('projectPlaceholder')}
-						class="mt-1 w-full resize-y border-none bg-slate-200 p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
-						bind:value={message}
-					></textarea>
-				</label>
-			</div>
-
-			<div class="flex self-center sm:gap-4">
-				<button
-					class="mt-5 rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600"
-					type="submit"
-				>
-					{translate('sendButton')}
-				</button>
-			</div>
-		</form>
+		<Card.Root class="w-[350px] self-center">
+			<Card.Header>
+				<Card.Title>{translate('contactUs')}</Card.Title>
+				<Card.Description>{translate('responseMessage')}</Card.Description>
+			</Card.Header>
+			<Card.Content>
+				<form on:submit={handleSubmit} autocomplete="off">
+					<div class="grid w-full items-center gap-4">
+						<div class="flex flex-col space-y-1.5">
+							<Label for="name">{translate('fullName')}</Label>
+							<Input
+								id="name"
+								type="text"
+								placeholder={translate('fullNamePlaceholder')}
+								bind:value={name}
+							/>
+						</div>
+						<div class="flex flex-col space-y-1.5">
+							<Label for="email">{translate('email')}</Label>
+							<Input
+								id="email"
+								type="email"
+								placeholder={translate('emailPlaceholder')}
+								bind:value={email}
+							/>
+						</div>
+						<div class="flex flex-col space-y-1.5">
+							<Label for="phone">{translate('phoneNumber')}</Label>
+							<Input
+								id="phone"
+								type="tel"
+								placeholder={translate('phonePlaceholder')}
+								bind:value={phone}
+							/>
+						</div>
+						<div class="flex flex-col space-y-1.5">
+							<Label for="projects">{translate('choose_service')}</Label>
+							<Select.Root>
+								<Select.Trigger id="projects" bind:value={project}>
+									<Select.Value placeholder="Select" />
+								</Select.Trigger>
+								<Select.Content>
+									{#each services as framework}
+										<Select.Item value={framework.value} label={framework.label}
+											>{framework.label}</Select.Item
+										>
+									{/each}
+								</Select.Content>
+							</Select.Root>
+						</div>
+						<div class="flex flex-col space-y-1.5">
+							<Label for="message">{translate('project')}</Label>
+							<Textarea
+								id="message"
+								placeholder={translate('projectPlaceholder')}
+								bind:value={message}
+							/>
+						</div>
+					</div>
+					<div class="mt-4 flex justify-between">
+						<Button variant="outline" type="button" on:click={clearForm}
+							>{translate('cancelButton')}</Button
+						>
+						<Button type="submit">{translate('sendButton')}</Button>
+					</div>
+				</form>
+			</Card.Content>
+		</Card.Root>
 	{/if}
 </div>
